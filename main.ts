5b114352fa6bdc292a85c470fa7324c378d712cc
Ethan Battaglia <56133501+EthansGuild@users.noreply.github.com>
2025-06-12 16:31:27 -0400

    started work on the shop system

--- main.ts
+++ main.ts
@@ -1,8 +1,9 @@
 namespace SpriteKind {
+    export const ShopText = SpriteKind.create()
     export const NoInteractions = SpriteKind.create()
 }
 sprites.onCreated(SpriteKind.Enemy, function (sprite) {
-    timer.after(10000, function () {
+    timer.after(enemyLifespan, function () {
         if (sprites.readDataBoolean(sprite, "squishable")) {
             sprite.sayText("bye!")
             sprites.setDataBoolean(sprite, "squishable", false)
@@ -16,9 +17,34 @@ sprites.onCreated(SpriteKind.Enemy, function (sprite) {
         }
     })
 })
+function setupShopUpgrades () {
+    isInShop = false
+    playerGravity = 4
+    spawnRate = 1
+    scoreMultiplier = 1
+    luckySquishRate = 9
+    enemyLifespan = 10000
+    chainReaction = false
+    myEffect = extraEffects.createCustomSpreadEffectData(
+    extraEffects.createPresetColorTable(ExtraEffectPresetColor.Fire),
+    false,
+    extraEffects.createPresetSizeTable(ExtraEffectPresetShape.Explosion),
+    extraEffects.createPercentageRange(50, 100),
+    extraEffects.createPercentageRange(50, 100),
+    extraEffects.createTimeRange(200, 400)
+    )
+    shopText = sprites.create(assets.image`shopText`, SpriteKind.NoInteractions)
+    shopText.setPosition(80, 448)
+    animation.runMovementAnimation(
+    shopText,
+    animation.animationPresets(animation.bobbing),
+    5000,
+    true
+    )
+}
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     if (mySprite.isHittingTile(CollisionDirection.Bottom)) {
-        mySprite.vy = -130
+        mySprite.vy = 0 - mySprite.ay / 3
     }
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`lv2Forward`, function (sprite, location) {
@@ -73,6 +99,48 @@ function flip (sprite: Sprite) {
     }
     sprite.setImage(spriteImage)
 }
+function updateGravityShopText () {
+    gravTitle = textsprite.create("Player Gravity")
+    // Update player gravity texts
+    if (playerGravity == 1) {
+        gravLevel = textsprite.create("Lv 1/4: 1x")
+        gravUpgrade = textsprite.create("Buy 1.5x for 100?")
+    } else if (playerGravity == 2) {
+        gravLevel = textsprite.create("Lv 2/4: 1.5x")
+        gravUpgrade = textsprite.create("Buy 2x for 500?")
+    } else if (playerGravity == 3) {
+        gravLevel = textsprite.create("Lv 3/4: 2x")
+        gravUpgrade = textsprite.create("Buy 2.5x for 3000?")
+    } else if (playerGravity == 4) {
+        gravLevel = textsprite.create("Lv 4/4: 2.5x")
+        gravUpgrade = textsprite.create("MAXED", 0, 7)
+    }
+    gravTitle.setKind(SpriteKind.ShopText)
+    gravLevel.setKind(SpriteKind.ShopText)
+    gravUpgrade.setKind(SpriteKind.ShopText)
+    tiles.placeOnTile(gravTitle, tiles.getTileLocation(11, 27))
+    tiles.placeOnTile(gravLevel, tiles.getTileLocation(11, 28))
+    tiles.placeOnTile(gravUpgrade, tiles.getTileLocation(11, 30))
+}
+function scorePopup (text: string, x: number, y: number) {
+    textSprite = textsprite.create(text)
+    textSprite.setOutline(1, 15)
+    textSprite.setPosition(x, y)
+    textSprite.ay = 40
+    textSprite.vy = -40
+    textSprite.lifespan = 1000
+}
+scene.onOverlapTile(SpriteKind.Player, assets.tile`shop`, function (sprite, location) {
+    returnPoint = tiles.getTileLocation(location.column, location.row + 2)
+    isInShop = true
+    sprites.destroyAllSpritesOfKind(SpriteKind.Enemy)
+    music.stopAllSounds()
+    music.play(music.createSong(assets.song`shopTheme`), music.PlaybackMode.LoopingInBackground)
+    tiles.setCurrentTilemap(tilemap`level2`)
+    tiles.placeOnTile(mySprite, tiles.getTileLocation(3, 31))
+    mySprite.setVelocity(0, 0)
+    updateAllShopText()
+})
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.stopAnimation(animation.AnimationTypes.All, mySprite)
     if (controller.right.isPressed()) {
@@ -112,6 +180,23 @@ controller.left.onEvent(ControllerButtonEvent.Released, function () {
         mySprite.setImage(assets.image`idle`)
     }
 })
+function updateAllShopText () {
+    sprites.destroyAllSpritesOfKind(SpriteKind.ShopText)
+    updateGravityShopText()
+    updateSpawnRateShopText()
+}
+function updateSpawnRateShopText () {
+    // Update spawn rate texts
+    if (spawnRate == 1) {
+    	
+    } else if (spawnRate == 2) {
+    	
+    } else if (spawnRate == 3) {
+    	
+    } else if (spawnRate == 4) {
+    	
+    }
+}
 scene.onOverlapTile(SpriteKind.Player, assets.tile`lv4Forward`, function (sprite, location) {
     scene.setBackgroundImage(assets.image`lv4BG`)
     playTheme(4)
@@ -154,10 +239,24 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
         )
     }
 })
+info.onScore(1, function () {
+	
+})
 scene.onOverlapTile(SpriteKind.Player, assets.tile`lv3Forward`, function (sprite, location) {
     scene.setBackgroundImage(assets.image`lv3BG`)
     playTheme(3)
 })
+function setPlayerGravity () {
+    if (playerGravity == 1) {
+        mySprite.ay = 400
+    } else if (playerGravity == 2) {
+        mySprite.ay = 600
+    } else if (playerGravity == 3) {
+        mySprite.ay = 800
+    } else if (playerGravity == 4) {
+        mySprite.ay = 1000
+    }
+}
 scene.onOverlapTile(SpriteKind.Player, assets.tile`tutBack`, function (sprite, location) {
     playTheme(0)
 })
@@ -168,6 +267,14 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`lv1Back`, function (sprite, l
 scene.onOverlapTile(SpriteKind.Player, assets.tile`lv3Back`, function (sprite, location) {
     scene.setBackgroundImage(assets.image`lv3BG`)
     playTheme(3)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`shopExit`, function (sprite, location) {
+    music.stopAllSounds()
+    playTheme(curLevel)
+    isInShop = false
+    tiles.setCurrentTilemap(tilemap`level1`)
+    tiles.placeOnTile(mySprite, returnPoint)
+    mySprite.setVelocity(0, 0)
 })
 info.onScore(20, function () {
     tiles.setTileAt(tiles.getTileLocation(43, 4), sprites.builtin.forestTiles10)
@@ -249,11 +356,21 @@ function setupLists () {
 }
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     if (sprites.readDataBoolean(otherSprite, "squishable") && (sprite.vy > 10 && sprite.bottom < otherSprite.y)) {
-        info.changeScoreBy(1)
-        sprite.vy = -100
+        if (Math.percentChance(luckySquishRate)) {
+            music.play(music.melodyPlayable(music.baDing), music.PlaybackMode.InBackground)
+            extraEffects.createSpreadEffectAt(myEffect, otherSprite.x, otherSprite.y, 200, 20)
+            pointsScored = Math.constrain(curLevel, 1, 4) * (scoreMultiplier * 10)
+            scorePopup("Lucky! +" + convertToText(pointsScored), otherSprite.x, otherSprite.y)
+        } else {
+            pointsScored = Math.constrain(curLevel, 1, 4) * scoreMultiplier
+            scorePopup("+" + convertToText(pointsScored), otherSprite.x, otherSprite.y)
+        }
+        info.changeScoreBy(pointsScored)
+        sprite.vy = -150
         squish(otherSprite)
     }
 })
+let pointsScored = 0
 let lastSelectedScream: music.Playable = null
 let randScreamList: music.SoundEffect[] = []
 let newlySelectedScream: music.Playable = null
@@ -264,25 +381,56 @@ let level1Enemies: Image[] = []
 let level0Enemies: Image[] = []
 let mySprite2: Sprite = null
 let curLevel = 0
+let returnPoint: tiles.Location = null
+let textSprite: TextSprite = null
+let gravUpgrade: TextSprite = null
+let gravLevel: TextSprite = null
+let gravTitle: TextSprite = null
 let spriteImage: Image = null
 let squishPrompt: Sprite = null
 let jumpPrompt: Sprite = null
 let partyText: Sprite = null
-let mySprite: Sprite = null
+let shopText: Sprite = null
+let myEffect: SpreadEffectData = null
+let chainReaction = false
+let luckySquishRate = 0
+let scoreMultiplier = 0
+let spawnRate = 0
+let playerGravity = 0
+let isInShop = false
+let enemyLifespan = 0
 let lastPlayedTheme = 0
+let mySprite: Sprite = null
 game.splash("garden stomper")
-lastPlayedTheme = 0
-setupTutorial()
-setupLists()
 scene.setBackgroundImage(assets.image`lv1BG`)
 tiles.setCurrentTilemap(tilemap`level1`)
+setupTutorial()
+setupShopUpgrades()
+setupLists()
 mySprite = sprites.create(assets.image`idle`, SpriteKind.Player)
 controller.moveSprite(mySprite, 100, 0)
-mySprite.ay = 400
-tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 4))
+mySprite.z = 999
+tiles.placeOnTile(mySprite, tiles.getTileLocation(100, 4))
 scene.cameraFollowSprite(mySprite)
+setPlayerGravity()
+scroller.scrollBackgroundWithCamera(scroller.CameraScrollMode.OnlyHorizontal)
+scroller.setCameraScrollingMultipliers(0.3, 0)
+lastPlayedTheme = 0
+playTheme(0)
+game.onUpdateInterval(250, function () {
+    if (spawnRate == 4 && !(isInShop)) {
+        makeEnemy()
+    }
+})
 game.onUpdateInterval(2000, function () {
-    makeEnemy()
+    if (spawnRate == 1 && !(isInShop)) {
+        makeEnemy()
+    }
+})
+game.onUpdateInterval(1000, function () {
+    if (spawnRate == 2 && !(isInShop)) {
+        makeEnemy()
+    }
 })
 // footsteps
 forever(function () {
@@ -290,5 +438,10 @@ forever(function () {
         timer.throttle("action", 200, function () {
             music.play(music.createSoundEffect(WaveShape.Sine, 123, 1, 255, 0, 100, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.InBackground)
         })
+    }
+})
+game.onUpdateInterval(500, function () {
+    if (spawnRate == 3 && !(isInShop)) {
+        makeEnemy()
     }
 })
